8a1a1aa47174ce7b242f7094508bafb626b40af8
swathisivalenka <swathisivalenka@gmail.com>
2025-06-30 12:26:00 +0530

    Update Animal.py

--- Animal.py
+++ Animal.py
@@ -3,7 +3,7 @@ class Animal:
         print("Animal speaks")
 class Dog(Animal): #access the parent class ka properties to this child class
     def bark(self):
-        print("Dog barks")
+        print("Dog barks bow bow")
 d = Dog()
 d.speak()
 d.bark()
